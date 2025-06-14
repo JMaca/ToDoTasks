e3a26051d230095515c2ddd86841e2307fd2459d
JMaca <70349528+JMaca@users.noreply.github.com>
2025-06-14 19:29:43 -0400

    Updated SecurityConfig.java -Added user details service for authentication
    -Configured Spring Security filter chain for HTTP request handling
    
    Updated TaskController.java
    -Added method to retrieve all tasks from the database
    
    Updated pom.xml
    -Added dependencies for PostgreSQL and Jackson (for LocalDate serialization)
    
    Updated application.properties
    -Configured PostgreSQL, JPA, Hibernate settings, and Spring Security credentials
    
    Added docker-compose.yml and .env.example
    - For local PostgreSQL setup and environment variable configuration

--- ToDoTasks/src/main/java/org/example/ToDoTasks/config/SecurityConfig.java
+++ ToDoTasks/src/main/java/org/example/ToDoTasks/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package org.example.ToDoTasks.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // tells Spring this will create and manage beans.
 public class SecurityConfig {
+
+    @Bean // object - class instance.
+    public UserDetailsService users() { // retrieve user info for authentication.
+        UserDetails user = User.builder()
+                .username("myuser")
+                .password("{noop}mypassword") // noop uses plain text (not secure, only for testing purposes).
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user); // Stores user in memory.
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // Spring securtiy handling HTTP requests.
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF protection is for browser-based sessions ( would block POST/GET requests).
+                .authorizeHttpRequests(auth -> auth // Requires authentication on all requests.
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // Basic authentication in the Authorization header
+        return http.build(); // build and return bean
+    }
 }
--- ToDoTasks/src/main/java/org/example/ToDoTasks/controller/TaskController.java
+++ ToDoTasks/src/main/java/org/example/ToDoTasks/controller/TaskController.java
@@ -1,17 +1,21 @@
 package org.example.ToDoTasks.controller;
 
 import org.example.ToDoTasks.dto.TaskDTO;
+import org.example.ToDoTasks.mapper.TaskMapper;
+import org.example.ToDoTasks.model.Task;
 import org.example.ToDoTasks.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // Tells Spring that this class handles RESTful requests, and return values in JSON form.
 @RequestMapping("/tasks") // Specifies base URL endpoint for this controller
 public class TaskController {
 
-    @Autowired // Injects class ( dependency injection ) Spring creates the required object.
+    @Autowired // Injects class (dependency injection) Spring creates the required object.
     private TaskService taskService;
 
     //CRUD methods Create, Read, Update, Delete - HTTP status = 2XX success, 4XX Client Error, 5XX Server Error
@@ -24,7 +28,16 @@ public class TaskController {
     // GET method (Read)
     @GetMapping("/{id}")
     public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id) {
-        return ResponseEntity.ok(taskService.getTaskById(id));
+        return ResponseEntity.ok(taskService.getTaskById(id)); // Call taskService to get a single task with matching ID
+    }
+
+    @GetMapping// GET request
+    public ResponseEntity<List<TaskDTO>> getAllTasks() {
+        List<Task> tasks = taskService.getAllTasks(); // call taskService to get all tasks from the database.
+        List<TaskDTO> taskDTOs = tasks.stream() // convert all tasks objects to TaskDTO objects.
+                .map(TaskMapper::toDTO)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(taskDTOs); // HTTP status
     }
 
     // PUT method (Update)
